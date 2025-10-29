ef40dfb370b22c8f626eeb2c7336b58a95a406c6
flamboh <oliver.boorstein@gmail.com>
2025-10-29 14:04:02 -0700

    array jobs results and associated source files

--- homework01/pi.c
+++ homework01/pi.c
@@ -14,7 +14,8 @@ void usage(int argc, char** argv);
 double calcPi_Serial(int num_steps);
 double calcPi_P1Atomic(int num_steps);
 double calcPi_P1Critical(int num_steps);
-double calcPi_P2(int num_steps);
+double calcPi_P2Atomic(int num_steps);
+double calcPi_P2Critical(int num_steps);
 
 
 int main(int argc, char** argv)
@@ -27,7 +28,7 @@ int main(int argc, char** argv)
         usage(argc, argv);
         printf("using %"PRIu32"\n", num_steps);
     }
-    fprintf(stdout, "The first 10 digits of Pi are %0.10f\n", PI);
+    // fprintf(stdout, "The first 10 digits of Pi are %0.10f\n", PI);
 
 
     // set up timer
@@ -35,39 +36,41 @@ int main(int argc, char** argv)
     uint64_t end_t;
     InitTSC();
 
-
+    // printf("num_threads,num_steps,time_serial,time_atomic,time_critical,time_monte_carlo_atomic,time_monte_carlo_critical,pi_serial,pi_atomic,pi_critical,pi_monte_carlo_atomic,pi_monte_carlo_critical\n");
+    printf("%d,%d,", omp_get_max_threads(), num_steps);
     // calculate in serial 
     start_t = ReadTSC();
     double Pi0 = calcPi_Serial(num_steps);
     end_t = ReadTSC();
-    printf("Time to calculate Pi serially with %"PRIu32" steps is: %g\n",
-           num_steps, ElapsedTime(end_t - start_t));
-    printf("Pi is %0.10f\n", Pi0);
-    
+    printf("%g,", ElapsedTime(end_t - start_t));
+        
     // calculate in parallel with integration
     start_t = ReadTSC();
     double Pi1A = calcPi_P1Atomic(num_steps);
     end_t = ReadTSC();
-    printf("Time to calculate Pi in parallel using atomic with %"PRIu32" steps is: %g\n",
-           num_steps, ElapsedTime(end_t - start_t));
-    printf("Pi is %0.10f\n", Pi1A);
-
+    printf("%g,", ElapsedTime(end_t - start_t));
+    
     // calculate in parallel with integration
     start_t = ReadTSC();
     double Pi1C = calcPi_P1Critical(num_steps);
     end_t = ReadTSC();
-    printf("Time to calculate Pi in in parallel using critical with %"PRIu32" steps is: %g\n",
-           num_steps, ElapsedTime(end_t - start_t));
-    printf("Pi is %0.10f\n", Pi1C);
-
-
-    // calculate in parallel with Monte Carlo
+    printf("%g,", ElapsedTime(end_t - start_t));
+        
+    // calculate in parallel with Monte Carlo atomic
     start_t = ReadTSC();
-    double Pi2 = calcPi_P2(num_steps);
+    double Pi2A = calcPi_P2Atomic(num_steps);
     end_t = ReadTSC();
-    printf("Time to calculate Pi in parallel using Monte Carlo with %"PRIu32" guesses is: %g\n",
-           num_steps, ElapsedTime(end_t - start_t));
-    printf("Pi is %0.10f\n", Pi2);
+    printf("%g,", ElapsedTime(end_t - start_t));
+    // calculate in parallel with Monte Carlo critical
+    start_t = ReadTSC();
+    double Pi2C = calcPi_P2Critical(num_steps);
+    end_t = ReadTSC();
+    printf("%g,", ElapsedTime(end_t - start_t));
+    printf("%0.10f,", Pi0);
+    printf("%0.10f,", Pi1A);
+    printf("%0.10f,", Pi1C);
+    printf("%0.10f,", Pi2A);
+    printf("%0.10f\n", Pi2C);
 
     
     return 0;
@@ -123,9 +126,24 @@ double calcPi_P1Critical(int num_steps)
     return pi * 2.0;
 }
 
-double calcPi_P2(int num_steps)
+double calcPi_P2Atomic(int num_steps)
 {
-    double pi = 0.0;
+    int num_in = 0;
+    #pragma omp parallel for
+    for (int i = 0; i < num_steps; i++) {
+        double x = (double)rand() / RAND_MAX;
+        double y = (double)rand() / RAND_MAX;
+
+        if (sqrt((x*x) + (y*y)) < 1) {
+            #pragma omp atomic
+            num_in++;
+        }
+    }
+    return (((float)num_in) / num_steps) * 4;
+}
+
+double calcPi_P2Critical(int num_steps)
+{
     int num_in = 0;
     #pragma omp parallel for
     for (int i = 0; i < num_steps; i++) {
