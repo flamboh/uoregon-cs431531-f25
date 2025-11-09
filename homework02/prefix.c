03c789ebcbe3bbfd9c8b44f4b60564a3a1ffbcb7
flamboh <oliver.boorstein@gmail.com>
2025-11-08 22:28:11 -0800

    update to output csv on optional flag for prefix, change both compilers to talapas compatible

--- homework02/prefix.c
+++ homework02/prefix.c
@@ -14,18 +14,45 @@ void prefix_sum(int* src, int* prefix, int n);
 void prefix_sum_p1(int* src, int* prefix, int n);
 void prefix_sum_p2(int* src, int* prefix, int n);
 
+static int csv_output_enabled = 0;
+
 
 int main(int argc, char** argv)
 {
     // get inputs
     uint32_t n = 1048576;
     unsigned int seed = time(NULL);
-    if(argc > 2) {
-        n = atoi(argv[1]); 
-        seed = atoi(argv[2]);
-    } else {
+    int have_n = 0;
+    int have_seed = 0;
+
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "--csv") == 0 || strcmp(argv[i], "-c") == 0) {
+            csv_output_enabled = 1;
+            continue;
+        }
+
+        if(!have_n) {
+            n = (uint32_t)atoi(argv[i]);
+            have_n = 1;
+            continue;
+        }
+
+        if(!have_seed) {
+            seed = (unsigned int)atoi(argv[i]);
+            have_seed = 1;
+            continue;
+        }
+
         usage(argc, argv);
-        printf("using %"PRIu32" elements and time as seed\n", n);
+        fprintf(stderr, "Too many positional arguments.\n");
+        return EXIT_FAILURE;
+    }
+
+    if(!(have_n && have_seed)) {
+        if(!csv_output_enabled) {
+            usage(argc, argv);
+            printf("using %"PRIu32" elements and time as seed\n", n);
+        }
     }
 
 
@@ -48,8 +75,11 @@ int main(int argc, char** argv)
     start_t = ReadTSC();
     prefix_sum(input_array, prefix_array, n);
     end_t = ReadTSC();
-    printf("Time to do O(N-1) prefix sum on a %"PRIu32" elements: %g (s)\n", 
-           n, ElapsedTime(end_t - start_t));
+    double serial_time = ElapsedTime(end_t - start_t);
+    if(!csv_output_enabled) {
+        printf("Time to do O(N-1) prefix sum on a %"PRIu32" elements: %g (s)\n",
+               n, serial_time);
+    }
 
 
     // execute parallel prefix sum which uses a NlogN algorithm
@@ -59,8 +89,11 @@ int main(int argc, char** argv)
     start_t = ReadTSC();
     prefix_sum_p1(input_array1, prefix_array1, n);
     end_t = ReadTSC();
-    printf("Time to do O(NlogN) //prefix sum on a %"PRIu32" elements: %g (s)\n",
-           n, ElapsedTime(end_t - start_t));
+    double nlogn_time = ElapsedTime(end_t - start_t);
+    if(!csv_output_enabled) {
+        printf("Time to do O(NlogN) //prefix sum on a %"PRIu32" elements: %g (s)\n",
+               n, nlogn_time);
+    }
     verify(prefix_array, prefix_array1, n);
 
     
@@ -70,9 +103,17 @@ int main(int argc, char** argv)
     start_t = ReadTSC();
     prefix_sum_p2(input_array1, prefix_array1, n);
     end_t = ReadTSC();
-    printf("Time to do 2(N-1) //prefix sum on a %"PRIu32" elements: %g (s)\n", 
-           n, ElapsedTime(end_t - start_t));
+    double two_n_time = ElapsedTime(end_t - start_t);
+    if(!csv_output_enabled) {
+        printf("Time to do 2(N-1) //prefix sum on a %"PRIu32" elements: %g (s)\n",
+               n, two_n_time);
+    }
     verify(prefix_array, prefix_array1, n);
+
+    if(csv_output_enabled) {
+        printf("serial,nlogn,2nminus1\n");
+        printf("%g,%g,%g\n", serial_time, nlogn_time, two_n_time);
+    }
 
 
     // free memory
@@ -87,7 +128,7 @@ int main(int argc, char** argv)
 
 void usage(int argc, char** argv)
 {
-    fprintf(stderr, "usage: %s <# elements> <rand seed>\n", argv[0]);
+    fprintf(stderr, "usage: %s <# elements> <rand seed> [-c|--csv]\n", argv[0]);
 }
 
 
@@ -101,7 +142,7 @@ void verify(int* sol, int* ans, int n)
     }
     if(err != 0) {
         fprintf(stderr, "There was an error: %d\n", err);
-    } else {
+    } else if(!csv_output_enabled) {
         fprintf(stdout, "Pass\n");
     }
 }
@@ -180,6 +221,3 @@ void prefix_sum_p2(int* src, int* prefix, int n)
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < n; i++) prefix[i] = temp[i] + src[i];
 }
-
-
-
